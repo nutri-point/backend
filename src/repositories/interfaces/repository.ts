949a95f189b6cf459049e022c41bd5ef25d255b2
dKustura <denis_kustura@hotmail.com>
2021-02-21 22:15:27 +0100

    🎨 Rename utility types

--- src/repositories/interfaces/repository.ts
+++ src/repositories/interfaces/repository.ts
@@ -1,11 +1,11 @@
+type AddType<T> = Omit<T, 'id' | 'createdAt' | 'updatedAt'>;
+
+type UpdateType<T> = Partial<AddType<T>>;
+
 interface IRepository<TEntity, TKey> {
   getAll(): Promise<Partial<TEntity>[]>;
   getById(id: TKey): Promise<TEntity>;
-  add(entity: EntityAdd<TEntity>): Promise<TEntity>;
-  update(id: TKey, entity: EntityUpdate<TEntity>): Promise<TEntity>;
+  add(entity: AddType<TEntity>): Promise<TEntity>;
+  update(id: TKey, entity: UpdateType<TEntity>): Promise<TEntity>;
   delete(id: TKey): Promise<TEntity>;
 }
-
-type EntityAdd<T> = Omit<T, 'id' | 'createdAt' | 'updatedAt'>;
-
-type EntityUpdate<T> = Partial<EntityAdd<T>>;
